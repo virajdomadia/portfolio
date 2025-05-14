b7d5241d79b2e8c0c134ad3db7f5fa5e471b5ae8
Viraj <virajdomadia32@gmail.com>
2025-05-14 23:19:55 +0530

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
